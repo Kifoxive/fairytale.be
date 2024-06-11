d5ad7c86326e8af678536e3cc74e91f0e2021d6c
Kifoxive <palianycia@seznam.cz>
2024-06-11 21:55:07 +0200

    Netlify functions test 2

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,11 +1,5 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import app from "../../src/app";
 
-const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
