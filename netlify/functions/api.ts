75000d4a68661f124de3414b7154d34f9ffaec0e
Kifoxive <palianycia@seznam.cz>
2024-06-11 22:29:56 +0200

    Netlify functions test 4

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,15 +4,28 @@ import app from "../../src/app";
 
 const PORT = process.env.PORT || 3000;
 
-export const handler = serverless(async () => {
+// export const handler = serverless(async () => {
+//   await mongoose
+//     .connect(process.env.DB_URL, {
+//       // useNewUrlParser: true,
+//       // useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("Connected to database"));
+//   app.listen(PORT, function () {
+//     console.log("Started application on port %d", PORT);
+//   });
+//   return app;
+// });
+
+export const handler = async (event, context) => {
+  // you can do other things here
   await mongoose
     .connect(process.env.DB_URL, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
     })
     .then(() => console.log("Connected to database"));
-  app.listen(PORT, function () {
-    console.log("Started application on port %d", PORT);
-  });
-  return app;
-});
+  const result = await serverless(app)(event, context);
+  // and here
+  return result;
+};
